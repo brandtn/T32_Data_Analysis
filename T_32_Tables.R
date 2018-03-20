f9633a70bd3f1cf5f4d45e0df6e3a99e87da187b
Nathan Brandt <Brandt@172-17-26-216.DYNAPOOL.NYU.EDU>
2018-03-20 17:14:07 -0400

    Sperating Files -Not quite yet -Figure Out Data Compling First -Just Do Manually

--- T_32_Tables.R
+++ T_32_Tables.R
@@ -1,9 +1,13 @@
 #Load libraries
 library("tidyverse")
 library("googlesheets")
+#GoogleSheets Info
+#https://cran.r-project.org/web/packages/googlesheets/vignettes/basic-usage.html
 
-#Function to convert university date code into decipherable terms
-convertdate <- function(x) {
+
+
+#Function to convert university date code into decipherable terms, returns the year
+convertdate_year <- function(x) {
 
   if (is.na(x)) {return(x)}
   #first charater is either 0 or 1, 0 = 19, 1 = 20
@@ -11,39 +15,67 @@ convertdate <- function(x) {
   #Last character is the semester, save to second vector spot
   
   if (substr(x, 1, 1) == 0) {
-    date <- paste(19, substr(x, 2, 3), sep = "")#, substr(x, 4, 4)
+    date <- paste(19, substr(x, 2, 3), sep = "")
   } else if (substr(x, 1, 1) == 1) {
-    date <- paste(20, substr(x, 2, 3), sep = "")#, substr(x, 4, 4)
+    date <- paste(20, substr(x, 2, 3), sep = "")
   }
-  # Last/Fourth character conver to semseter, 2 = Winter, 4 = Spring, 6 = Summer, 8 = Fall
- # if (date[2] == 2) { date[2] <- 'Winter'
- # } else if (date[2] == 4) {date[2] <- 'Spring'
- # } else if (date[2] == 6) {date[2] <- 'Summer'
- # } else if (date[2] == 8) {date[2] <- 'Fall'
- # } 
-  #return date
   return(as.numeric(date))
 }
+
+#Function to convert university date code into decipherable terms, returns the semester
+#May not need
+convertdate_semester <- function(x) {  
+  if (is.na(x)) {return(x)}
+  
+  # Last/Fourth character conver to semseter, 2 = Winter, 4 = Spring, 6 = Summer, 8 = Fall
+  if (substr(x, 4, 4)) { date <- 'Winter'
+  } else if (substr(x, 4, 4)) {date <- 'Spring'
+  } else if (substr(x, 4, 4)) {date <- 'Summer'
+  } else if (substr(x, 4, 4)) {date <- 'Fall'
+  } 
+  #return date
+  return(as.character(date))
+}
+
+
+
+#Reads in list of PIs
+pi.list <- select(gs_read(ss = gs_title("PI_Contact_Info_Tracker")), PI)
+#Adds in first
+t32 <- gs_read(ss = gs_title(paste("T_32_PhD_Info_",pi.list[1, 1], sep = "")))
+#goes through the rest
+for(x in 2:length(pi.list$PI)) {
+   t32 <- bind_rows(t32, gs_read(ss = gs_title(paste("T_32_PhD_Info_",pi.list[x, 1], sep = ""))))
+}
+
+
+# Not working as intended.
+#Possile Stratergy. Compile data. Use Table names to delete all but
+t32.new <- left_join(gs_read(ss = gs_title("T_32")), t32)
+
 
 #read in data from Google Sheet
 #First Time requires the input of an authroazation key
 student_data <- gs_read(ss = gs_title("T_32"))
 
-#Remove Unnessary Colum
+
+
+#Remove Unnessary Column
 student_data <- student_data %>% select(-Student_Name_1)
 
-#Convert to Usable Dates
-student_data$First_Term <- sapply(student_data$First_Term, convertdate)
-student_data$Most_Recent <- sapply(student_data$Most_Recent, convertdate)
-student_data$GABIOLMS <- sapply(student_data$GABIOLMS, convertdate)
-student_data$GABIOLPHD <- sapply(student_data$GABIOLPHD, convertdate)
+#Convert to Usable Dates - Years only for now
+student_data$First_Term <- sapply(student_data$First_Term, convertdate_year)
+student_data$Most_Recent <- sapply(student_data$Most_Recent, convertdate_year)
+student_data$GABIOLMS <- sapply(student_data$GABIOLMS, convertdate_year)
+student_data$GABIOLPHD <- sapply(student_data$GABIOLPHD, convertdate_year)
 
+#Corrects for later analysis
 student_data$Citz_Stat_Cd <- as.factor(student_data$Citz_Stat_Cd)
 
 
 glimpse(student_data)
 
-
+#global filter parameter
 filter.year <- 2000
 
 #How many students per year? (dot and line plot)
