64d0bf7b2003bf5c4abfd8dfeeca27d3c91d25a9
Nathan Brandt <Brandt@172-17-26-216.DYNAPOOL.NYU.EDU>
2018-03-20 12:14:05 -0400

    Working Date Converter

--- T_32_Tables.R
+++ T_32_Tables.R
@@ -8,6 +8,41 @@ student_data <- gs_read(ss = gs_title("T_32"))
 glimpse(student_data)
 
 
+#How many students per year? (dot and line plot)
+ggplot(student_data, aes(????)) +
+  geom_point() +
+  geom_line()
+
+#Demographics (pie or barcharts)
+  #Citizenship status
+  #Country of citizenship
+  #Ethnicity    
+  #Qualifications upon entry to program
+  #GRE scores
+
+#PhD experience (histograms or boxplot with points overlayed)
+  #Time to degree
+  #Number of semesters of teaching
+  #Number of published papers
+    #1st author
+    #co-author
+  #Subsequent position (pie or barplot)
+    #Postdoc vs not postdoc
+    #Postdoc institution
+    #Summary of employment
+  #Test predictors of PhD experience
+    #GRE vs number of papers (scatter plot)
+    #citizenship/ethnicity vs number of papers (boxplots)
+    #Number of papers vs postdoc or not postdoc (boxplots)
+    #Semesters teaching vs number of papers (boxplots)
+
+#PI commitment
+  #Number of students per PI (histogram)
+  #Number of student co-mentored (class within the above histogram or no plot)
+  #Number of PIs in co-mentoring relationships
+  #Distribution of committee membership (how many committees is each PI on) (histogram)
+
+
 ggplot(student_data, aes(First_Term)) +
   geom_bar()
 
